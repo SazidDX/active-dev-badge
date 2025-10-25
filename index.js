26b1786f67842cd9b62460cd4b158750ac9f9f47
SazidDX <sazid2007yr@gmail.com>
2025-10-25 13:39:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
         .setColor('#00D4AA')
         .setThumbnail('https://cdn.discordapp.com/attachments/1395245783808348331/1400354191624372375/0d02b202baf618dc122475bf70350fd9.png') 
         .setFooter({ 
-          text: '🔥 Active Developer Badge bot | Made with ❤️ by Manish',
+          text: '🔥 Active Developer Badge bot | Made with ❤️ by SDX',
           iconURL: 'https://cdn.discordapp.com/attachments/1332936607267033138/1400353273906593844/image_8.png' 
         })
         .setTimestamp()
